076f872583d9c8bf5a42c74eb3d2c9a6521c3304
himax-01 <himanshu1209.pandey@gmail.com>
2024-07-24 07:57:26 +0000

    more type conv

--- 01_basics/03_typeconversion.js
+++ 01_basics/03_typeconversion.js
@@ -1,10 +1,10 @@
 let score = 10
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let stScore = String(score)
 
-console.log(typeof(stScore)); 
+// console.log(typeof(stScore)); 
 
 //conversion to number:
 // "33" ==> 33
@@ -12,4 +12,10 @@ console.log(typeof(stScore));
 // true ==> 1
 //false ==> 0
 //null ==> 0
+ 
+let num = 2
+let negNum = -num;
+console.log(negNum);
+console.log(typeof negNum);
+
  
