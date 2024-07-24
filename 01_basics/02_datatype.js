5674a883c03f3600ae31b1ee02b42f465299a4a6
himax-01 <himanshu1209.pandey@gmail.com>
2024-07-24 06:54:19 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,18 @@
+"use strict"; // treat JS as newer version
+
+// alert(3+3) // we are using node.js not browser
+
+//code redability should be high
+
+let name = "himansu"
+let age = 10
+let isLoggedIn = false
+//type of datatype
+//null is also a datatype, standalone value
+//undefined is empty value
+//symbol => used for uniqueness
+
+//object
+
+console.log(typeof "himanshu");
+console.log(typeof null); //object
