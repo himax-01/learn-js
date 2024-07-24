e6c66f694e23ea134fd56325c1acae4566e45482
himax-01 <himanshu1209.pandey@gmail.com>
2024-07-24 07:38:51 +0000

    type conversion

---
+++ 01_basics/03_typeconversion.js
@@ -0,0 +1,15 @@
+let score = 10
+
+console.log(typeof score);
+
+let stScore = String(score)
+
+console.log(typeof(stScore)); 
+
+//conversion to number:
+// "33" ==> 33
+// "33abc"==> NaN but converted to number
+// true ==> 1
+//false ==> 0
+//null ==> 0
+ 
